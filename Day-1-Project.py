3f26c9eb05c5c44c3f7edbaee05a259ab60fead1
Karan Kumar <59556246+thekarankumar@users.noreply.github.com>
2023-07-28 18:05:51 +0530

    Add files via upload
    Day-1 Project: Band Name Generator
    
    Level: Beginner

---
+++ Day-1-Project.py
@@ -0,0 +1,14 @@
+# Day 1
+# Band Name Generator Project
+
+# Greet Message
+print("Hello, Welcome to Band Name Generator Project!\n\n\nIn order to suggest you a band name, I would like "
+      "to ask you a few questions.\n")
+
+# Taking name as input from the user
+name = input("Which city did you grew up in?\n")
+
+pet_name = input("What is your pet's name?\n")
+
+# Final output
+print("Your band name could be: " + name + " " + pet_name)
